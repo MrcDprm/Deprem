567e85a8278ff3a22e7001d7946c36d794b8d26a
mrcdp <mrcdp@MRCDPRM>
2024-10-23 09:19:13 +0300

    Add project files.

---
+++ DepremWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DepremWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

8f493bc33e89dbe722322d23e102663a899c0930
Miraç Deprem <mrcdprmm@hotmail.com>
2024-11-04 09:07:25 +0300

    deva

---
+++ Deprem.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Deprem.DataAccess.Data;
+using Deprem.DataAccess.Repository.IRepository;
+using Deprem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deprem.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db= db;
+        }
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ Deprem.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Deprem.DataAccess.Data;
+using Deprem.DataAccess.Repository.IRepository;
+using Deprem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deprem.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db= db;
+        }
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
